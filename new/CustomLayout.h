e6d68ed4f33a9483d62732426faf083060b96362
jimmy <jimmy@hwzdeMac-mini.local>
2016-08-17 10:47:11 +0800

    add a file

---
+++ new/CustomLayout.h
@@ -0,0 +1,13 @@
+//
+//  CustomLayout.h
+//  iOS-test
+//
+//  Created by jimmy on 16/8/5.
+//  Copyright © 2016年 cw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomLayout : UICollectionViewFlowLayout
+
+@end
