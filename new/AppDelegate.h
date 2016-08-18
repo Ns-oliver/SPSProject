e5594b97329ed8696b08cbf1cb8181d0469c4e59
Ns-oliver <272324636@qq.com>
2016-08-18 16:34:33 +0800

    update a file

---
+++ new/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kcnzqH5
+//
+//  Created by jimmy on 16/5/12.
+//  Copyright © 2016年 cw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
